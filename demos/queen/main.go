69cba8d3c786e3b5869684987a15cd6e5be847ca
sjnam <jsunam@gmail.com>
2026-06-12 06:45:24 +0900

    queen moves and wrapping

--- demos/queen/main.go
+++ demos/queen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"slices"
@@ -10,13 +11,25 @@ import (
 )
 
 func main() {
-	g, err := gbbasic.Board(3, 4, 0, 0, -1, 0, false)
+	pWrap := flag.Bool("w", false, "a bool")
+	flag.Parse()
+
+	wrap := int64(0)
+	gbName, cylind := "queen.gb", ""
+
+	if *pWrap {
+		wrap = int64(2)
+		gbName = "queen_wrap.gb"
+		cylind = "Cylindrical "
+	}
+
+	g, err := gbbasic.Board(3, 4, 0, 0, -1, wrap, false)
 	if err != nil {
 		fmt.Fprintf(os.Stderr,
 			"Sorry, I couldn't build a board g (%v)!\n", err)
 		os.Exit(1)
 	}
-	gg, err := gbbasic.Board(3, 4, 0, 0, -2, 0, false)
+	gg, err := gbbasic.Board(3, 4, 0, 0, -2, wrap, false)
 	if err != nil {
 		fmt.Fprintf(os.Stderr,
 			"Sorry, I couldn't build a board gg (%v)!\n", err)
@@ -28,17 +41,16 @@ func main() {
 			"Sorry, I couldn't build a gunion (%v)!\n", err)
 		os.Exit(1)
 	}
-
-	if _, err = gbsave.SaveGraph(ggg, "queen.gb"); err != nil {
+	if _, err = gbsave.SaveGraph(ggg, gbName); err != nil {
 		fmt.Fprintf(os.Stderr,
 			"Sorry, I couldn't save a graph (%v)!\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Print("Queen Moves on a 3x4 Board\n\n")
+	// print the vertices and edges of ggg
+	fmt.Printf("Queen Moves on a %s3x4 Board\n\n", cylind)
 	fmt.Printf("  The graph whose official name is\n%s\n", ggg.ID)
 	fmt.Printf("  has %d vertices and %d arcs:\n\n", ggg.N, ggg.M)
-
 	for v := range slices.Values(ggg.Vertices[:ggg.N]) {
 		fmt.Printf("%s\n", v.Name)
 		for a := range v.AllArcs() {
