c1e7b1169f4ea8c072d4e5ec2d850e70878d8e80
sjnam <jsunam@gmail.com>
2026-06-12 05:59:02 +0900

    detailed error msg

--- demos/queen/main.go
+++ demos/queen/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/sjnam/go-sgb/gbbasic"
 	"github.com/sjnam/go-sgb/gbsave"
@@ -11,28 +12,26 @@ import (
 func main() {
 	g, err := gbbasic.Board(3, 4, 0, 0, -1, 0, false)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Something went wrong %v", err)
+		fmt.Fprintf(os.Stderr,
+			"Sorry, I couldn't build a board g (%v)!\n", err)
 		os.Exit(1)
 	}
-
 	gg, err := gbbasic.Board(3, 4, 0, 0, -2, 0, false)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Something went wrong %v", err)
+		fmt.Fprintf(os.Stderr,
+			"Sorry, I couldn't build a board gg (%v)!\n", err)
 		os.Exit(1)
 	}
-
 	ggg, err := gbbasic.Gunion(g, gg, false, false)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Something went wrong %v", err)
-		os.Exit(1)
-	}
-	if ggg == nil {
-		fmt.Fprintf(os.Stderr, "Something went wrong %v", err)
+		fmt.Fprintf(os.Stderr,
+			"Sorry, I couldn't build a gunion (%v)!\n", err)
 		os.Exit(1)
 	}
 
 	if _, err = gbsave.SaveGraph(ggg, "queen.gb"); err != nil {
-		fmt.Fprintf(os.Stderr, "Something went wrong %v", err)
+		fmt.Fprintf(os.Stderr,
+			"Sorry, I couldn't save a graph (%v)!\n", err)
 		os.Exit(1)
 	}
 
@@ -40,7 +39,7 @@ func main() {
 	fmt.Printf("  The graph whose official name is\n%s\n", ggg.ID)
 	fmt.Printf("  has %d vertices and %d arcs:\n\n", ggg.N, ggg.M)
 
-	for _, v := range ggg.Vertices[:ggg.N] {
+	for v := range slices.Values(ggg.Vertices[:ggg.N]) {
 		fmt.Printf("%s\n", v.Name)
 		for a := range v.AllArcs() {
 			fmt.Printf(" -> %s, length %d\n", a.Tip.Name, a.Len)
